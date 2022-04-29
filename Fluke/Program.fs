45d7cb58ce8eafe273a2606689e84e355667d723
Spyobird <elliotlimzy@gmail.com>
2022-04-29 11:28:50 +0800

    Create initial commit

---
+++ Fluke/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let fluke = "Fluke"
+
+let mutable exit = false
+
+let formattedPrint message = 
+    printfn "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\
+        %s\n\
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" message
+
+let echo message =
+    formattedPrint message
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello from %s\n" fluke
+    while not exit do
+        let input = Console.ReadLine()
+        match input with
+        | "bye" -> exit <- true
+        | _ -> echo input
+    0
